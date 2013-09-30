df160767528b89a66627ba92c1e9e66e4d8233bb
Kirill Pavlov <kp@multichannel.net>
2013-09-30 21:43:33 +0800

    add testss

--- jsonrpc/tests/test_jsonrpc.py
+++ jsonrpc/tests/test_jsonrpc.py
@@ -20,9 +20,17 @@ class TestJSONRPCRequest(unittest.TestCase):
         with self.assertRaises(Exception):
             JSONRPCRequest("add")
 
-    def test_serialize(self):
+    def test_serialize_args_no_id(self):
         request = JSONRPCRequest("add", [1, 2])
         self.assertEqual(
             json.loads(request.json),
             {"method": "add", "params": [1, 2], "jsonrpc": "2.0"},
+        )
+
+    def test_serialize_kwargs_no_id(self):
+        request = JSONRPCRequest("devide", {"numerator": 1, "denominator": 2})
+        self.assertEqual(
+            json.loads(request.json),
+            {"method": "add", "params": {"numerator": 1, "denominator": 2},
+             "jsonrpc": "2.0"},
         )
