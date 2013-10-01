33457a1dac6840da120be3fb37840021040cb9e8
Kirill Pavlov <k@p99.io>
2013-10-02 01:25:07 +0800

    complete test examples

--- jsonrpc/tests/test_examples.py
+++ jsonrpc/tests/test_examples.py
@@ -40,6 +40,8 @@ class TestJSONRPCExamples(unittest.TestCase):
 
         dispatcher = {
             "subtract": subtract,
+            "sum": lambda *args: sum(args),
+            "get_data": lambda: ["hello", 5],
         }
 
         req = '{"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42}, "id": 3}'  # noqa
@@ -132,7 +134,29 @@ class TestJSONRPCExamples(unittest.TestCase):
         )
 
     def test_rpc_call_batch(self):
-        raise NotImplemented()
+        req = """[
+            {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+            {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
+            {"jsonrpc": "2.0", "method": "subtract",
+            "params": [42,23], "id": "2"},
+            {"foo": "boo"},
+            {"jsonrpc": "2.0", "method": "foo.get",
+            "params": {"name": "myself"}, "id": "5"},
+            {"jsonrpc": "2.0", "method": "get_data", "id": "9"}
+        ]"""
+        response = JSONRPCResponseManager.handle(req, self.dispatcher)
+        self.assertTrue(
+            response,
+            json.loads("""[
+                {"jsonrpc": "2.0", "result": 7, "id": "1"},
+                {"jsonrpc": "2.0", "result": 19, "id": "2"},
+                {"jsonrpc": "2.0", "error": {"code": -32600,
+                "message": "Invalid Request"}, "id": null},
+                {"jsonrpc": "2.0", "error": {"code": -32601,
+                "message": "Method not found"}, "id": "5"},
+                {"jsonrpc": "2.0", "result": ["hello", 5], "id": "9"}
+            ]""")
+        )
 
     def test_rpc_call_batch_all_notifications(self):
         req = """[
