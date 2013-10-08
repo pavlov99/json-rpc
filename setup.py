f091bd8d09ea5ac3d5bd7fa33ff7624330528e9b
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-10-08 04:02:43 +0000

    add python 3.3 to classifiers.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     license="MIT",
