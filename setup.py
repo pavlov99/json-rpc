56e636c95164c96de9802aa0df53f93c3bb0d218
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-11-09 15:27:52 +0000

    update setup long description info

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     author_email="kirill.pavlov@phystech.edu",
     url="https://github.com/pavlov99/json-rpc",
     description="JSON-RPC transport realisation",
+    long_description=read('README.md'),
 
     # Full list:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
