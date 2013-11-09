a7252710b3b5da5d7004771c6ad40dc35da88f44
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-11-09 11:35:00 +0000

    fix jsonrpc manager

--- jsonrpc/jsonrpc.py
+++ jsonrpc/jsonrpc.py
@@ -1,86 +1,24 @@
-﻿from .utils import JSONSerializable
+﻿""" JSON-RPC wrappers for version 1.0 and 2.0.
+
+Objects diring init operation try to choose JSON-RPC 2.0 and in case of error
+JSON-RPC 1.0.
+from_json methods could decide what format is it by presence of 'jsonrpc'
+attribute.
+
+"""
+from .utils import JSONSerializable
+from .jsonrpc1 import JSONRPC10Request
+from .jsonrpc2 import JSONRPC20Request
 
 
-class JSONRPCBaseRequest(JSONSerializable):
+class JSONRPCRequest(JSONSerializable):
 
-    """ Base class for JSON-RPC 1.0 and JSON-RPC 2.0 requests."""
-
-    def __init__(self, method=None, params=None, _id=None,
-                 is_notification=None):
-        self.data = dict()
-        self.method = method
-        self.params = params
-        self._id = _id
-        self.is_notification = is_notification
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, dict):
-            raise ValueError("data should be dict")
-
-        self._data = value
-
-    @property
-    def args(self):
-        """ Method position arguments.
-
-        :return tuple args: method position arguments.
-
-        """
-        return tuple(self.params) if isinstance(self.params, list) else ()
-
-    @property
-    def kwargs(self):
-        """ Method named arguments.
-
-        :return dict kwargs: method named arguments.
-
-        """
-        return self.params if isinstance(self.params, dict) else {}
-
-    @property
-    def json(self):
-        return self.serialize(self.data)
+    """ JSONRPC Request."""
 
     @classmethod
     def from_json(cls, json_str):
         data = cls.deserialize(json_str)
-
-        if not isinstance(data, dict):
-            raise ValueError("data should be dict")
-
-        return cls(**data)
-
-
-class JSONRPCBaseResponse(JSONSerializable):
-
-    """ Base class for JSON-RPC 1.0 and JSON-RPC 2.0 responses."""
-
-    def __init__(self, result=None, error=None, _id=None):
-        self.data = dict()
-
-        self.result = result
-        self.error = error
-        self._id = _id
-
-        if self.result is None and self.error is None:
-            raise ValueError("Either result or error should be used")
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, dict):
-            raise ValueError("data should be dict")
-
-        self._data = value
-
-    @property
-    def json(self):
-        return self.serialize(self.data)
+        if isinstance(data, dict) and "jsonrpc" not in data:
+            return JSONRPC10Request.from_json(json_str)
+        else:
+            return JSONRPC20Request.from_json(json_str)
