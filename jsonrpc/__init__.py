1b33c2355b9a4a021d917aaee092e883cdb3d5e5
Kirill Pavlov <k@p99.io>
2017-10-29 14:21:04 +0800

    REL: release minor update

--- jsonrpc/__init__.py
+++ jsonrpc/__init__.py
@@ -1,7 +1,7 @@
 from .manager import JSONRPCResponseManager
 from .dispatcher import Dispatcher
 
-__version = (1, 10, 3)
+__version = (1, 10, 4)
 
 __version__ = version = '.'.join(map(str, __version))
 __project__ = PROJECT = __name__
