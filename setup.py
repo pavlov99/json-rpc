a3295146fa5d86dfe541074fed1d1b8e368f0794
Kirill Pavlov <k@p99.io>
2018-12-29 16:51:26 +0800

    TST: use pytest in setup.py instead of nose

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-import os
+import os.path
+import sys
 from setuptools import setup, find_packages
 from jsonrpc import version
-#
-# Import multiprocessing to prevent test run problem. In case of nosetests
-# (not nose2) there is probles, for details see:
-# https://groups.google.com/forum/#!msg/nose-users/fnJ-kAUbYHQ/_UsLN786ygcJ
-# http://bugs.python.org/issue15881#msg170215w
-try:
-    import multiprocessing
-except ImportError:
-    pass
 
 
 def read(fname):
@@ -19,13 +11,16 @@ def read(fname):
     except IOError:
         return ""
 
+setup_requires = ["pytest-runner"] \
+    if {'pytest', 'test'}.intersection(sys.argv) \
+    else []
+
 setup(
     name="json-rpc",
     version=version,
     packages=find_packages(),
-    # test_suite="nose2.collector.collector",
-    test_suite="nose.collector",
-    tests_require=["nose", "mock"],
+    setup_requires=setup_requires,
+    tests_require=["pytest"],
 
     # metadata for upload to PyPI
     author="Kirill Pavlov",
